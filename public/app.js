6ecf8299a1e5d436eec8e04ec82aca7a2fe1d369
Sackah <81268132+Sackah@users.noreply.github.com>
2023-10-15 22:11:36 +0000

    updates

--- public/app.js
+++ public/app.js
@@ -1,1 +1,31 @@
 "use strict";
+const moreButton = document.querySelector('#more');
+const minusButton = document.querySelector('.fa-minus');
+const plusButton = document.querySelector('.fa-plus');
+const moreLinks = document.querySelector('.more-links');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const menutogle1 = document.querySelector('.mnu-tgl1');
+const menutogle2 = document.querySelector('.mnu-tgl2');
+minusButton.style.display = 'none';
+moreLinks.style.display = 'none';
+moreButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (minusButton.style.display === 'none') {
+        minusButton.style.display = 'inline';
+        plusButton.style.display = 'none';
+        moreLinks.style.display = 'block';
+    }
+    else {
+        minusButton.style.display = 'none';
+        plusButton.style.display = 'inline';
+        moreLinks.style.display = 'none';
+    }
+});
+menutogle1.addEventListener('click', (e) => {
+    e.preventDefault();
+    dropdownMenu.classList.remove('hide');
+});
+menutogle2.addEventListener('click', (e) => {
+    e.preventDefault();
+    dropdownMenu.classList.add('hide');
+});
---
+++ src/app.ts
@@ -0,0 +1,37 @@
+const moreButton = document.querySelector('#more') as HTMLAnchorElement;
+const minusButton = document.querySelector('.fa-minus') as HTMLElement;
+const plusButton = document.querySelector('.fa-plus') as HTMLElement;
+const moreLinks = document.querySelector('.more-links') as HTMLElement;
+const dropdownMenu = document.querySelector('.dropdown-menu') as HTMLElement;
+const menutogle1 = document.querySelector('.mnu-tgl1') as HTMLElement;
+const menutogle2 = document.querySelector('.mnu-tgl2') as HTMLElement;
+
+minusButton.style.display = 'none';
+moreLinks.style.display = 'none';
+
+moreButton.addEventListener('click', (e: Event) => {
+    e.preventDefault();
+
+    if(minusButton.style.display === 'none'){
+        minusButton.style.display = 'inline';
+        plusButton.style.display = 'none';
+        moreLinks.style.display = 'block'
+    }else {
+        minusButton.style.display = 'none';
+        plusButton.style.display = 'inline';
+        moreLinks.style.display = 'none';
+    }
+})
+
+menutogle1.addEventListener('click', (e: Event) => {
+    e.preventDefault();
+    dropdownMenu.classList.remove('hide');
+})
+
+menutogle2.addEventListener('click', (e: Event) => {
+    e.preventDefault();
+    dropdownMenu.classList.add('hide');
+})
+
+
+
